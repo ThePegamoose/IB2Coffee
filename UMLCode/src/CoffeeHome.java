5352e4b736f100c6e7906a03aaed83da559747a3
kris.ingelbrecht <kris.ingelbrecht@student.kuleuven.be>
2022-03-27 10:35:49 +0100

    Made frame a bit broader

--- UMLCode/src/CoffeeHome.java
+++ UMLCode/src/CoffeeHome.java
@@ -106,7 +106,7 @@ public class CoffeeHome extends JFrame{
         String water ="0";
         CoffeeHome  frame = new CoffeeHome( "CoffeeApp");
         frame.setVisible(true);
-        frame.setSize(500,300);
+        frame.setSize(505,300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         DB db = new DB();
         db.parseJSON(db.makeGETRequest( "https://studev.groept.be/api/a21ib2a04/waterlevel"));
